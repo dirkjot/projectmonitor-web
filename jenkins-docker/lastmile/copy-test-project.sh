e25bbcbf273777f9025caac13fbe8c3e7c8fd9ff
Dirk P. Janssen <djanssen@pivotal.io>
2017-05-08 21:41:20 -0700

    Intermediate commit towards jjb
    - Removed jenkins sandbox directory
    - Added jenkins job builder files

--- jenkins-docker/lastmile/copy-test-project.sh
+++ jenkins-docker/lastmile/copy-test-project.sh
@@ -3,25 +3,13 @@
 set -e
 
 JENKINS_HOME=/opt/jenkins
-
-mkdir -p ${JENKINS_HOME}/jobs 
-cp -fR /tmp/jenkins/jobs/* ${JENKINS_HOME}/jobs/  
-cp -f  /tmp/jenkins/hudson.tasks.Maven.xml ${JENKINS_HOME}
-
 pushd $JENKINS_HOME
+cp -f  lastmile/hudson.tasks.Maven.xml ${JENKINS_HOME}/
 patch < lastmile/config.xml.patch
 popd
 
 
 
--------------
 
-
-cd ${JENKINS_HOME}/lastmile
-tar xf jenkins.sb.tgz
-cp -fR ./jenkins/jobs/sandbox ./jenkins/jobs/testAnyFeature ./jenkins/jobs/testMaster ${JENKINS_HOME}/jobs/
-cp -f ./jenkins/hudson.tasks.Maven.xml ${JENKINS_HOME}/
-cd ${JENKINS_HOME}
-patch  < lastmile/config.xml.patch
 
 
